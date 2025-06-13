2655f46e31c7652a5be0ae19ded539739b316da6
elidcraig <elidcraig@gmail.com>
2025-06-13 14:08:02 -0400

    Initial commit

---
+++ test/vanity-numbers-generator.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as VanityNumbersGenerator from '../lib/vanity-numbers-generator-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/vanity-numbers-generator-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new VanityNumbersGenerator.VanityNumbersGeneratorStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
